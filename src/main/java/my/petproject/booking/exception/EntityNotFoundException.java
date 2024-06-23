196a20b144403e32a281c3ebef322ce223d6ab62
AgroFix <colalexm@gmail.com>
2024-06-23 18:52:30 +0300

    Fixed checkstyle

--- src/main/java/my/petproject/booking/exception/EntityNotFoundException.java
+++ src/main/java/my/petproject/booking/exception/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package my.petproject.booking.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message)  {
+    public EntityNotFoundException(String message) {
         super(message);
     }
 }
